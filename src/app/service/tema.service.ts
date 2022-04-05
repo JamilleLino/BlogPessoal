ea572dfa1043b24526363ec844da951de5ab9284
Jamille <lino.jamille@gmail.com>
2022-04-05 12:10:00 -0300

    Implementação do put e delete de temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { observable, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
 
@@ -16,12 +16,26 @@ headers: new HttpHeaders().set('Authorization', environment.token)
 }
 
 getAllTema(): Observable<Tema[]>{
-  return this.http.get<Tema[]>('https://blogpessoaljamille.herokuapp.com/temas', this.token)
+  return this.http.get<Tema[]>('https://blogpessoalthiago.herokuapp.com/temas', this.token)
 
 }
+
+getByIdTema(id: number): Observable<Tema>{
+return this.http.get<Tema>(`https://blogpessoalthiago.herokuapp.com/temas/${id}`, this.token)
+}
+
 
 postTema(tema: Tema): Observable<Tema>{
-return this.http.post<Tema>('https://blogpessoaljamille.herokuapp.com/temas', tema, this.token)
+return this.http.post<Tema>('https://blogpessoalthiago.herokuapp.com/temas', tema, this.token)
 }
+
+putTema(tema: Tema): Observable<Tema>{
+  return this.http.put<Tema>('https://blogpessoalthiago.herokuapp.com/temas', tema, this.token)
+}
+
+deleteTema(id: number){
+  return this.http.delete(`https://blogpessoalthiago.herokuapp.com/temas/${id}`, this.token)
+}
+
 
 }
